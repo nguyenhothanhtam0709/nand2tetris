2c84fa615800bb465b3746e045e338e16b16f7c5
tam.nguyen <tam.nguyen@gmail.com>
2025-07-28 13:45:07 +0700

    feat: basic sematic analyzer

--- hack-assembler/hack_assembler/semantic_analyzer.py
+++ hack-assembler/hack_assembler/semantic_analyzer.py
@@ -1,6 +1,6 @@
 from hack_assembler.ast_visitor import NodeVisitor
 from hack_assembler.ast import ProgramNode, AInstructionNode, CInstructionNode, SymbolDeclarationNode
-from hack_assembler.symbol_table import SymbolTable
+from hack_assembler.symbol_table import SymbolTable, DeclaredSymbol
 
 
 class SemanticAnalyzer(NodeVisitor):
@@ -22,10 +22,12 @@ class SemanticAnalyzer(NodeVisitor):
             self._visit(instruction_node)
 
     def _visit_AInstructionNode(self, node: AInstructionNode) -> None:
-        pass
+        self._current_line += 1
 
     def _visit_CInstructionNode(self, node: CInstructionNode) -> None:
-        pass
+        self._current_line += 1
 
     def _visit_SymbolDeclarationNode(self, node: SymbolDeclarationNode) -> None:
-        pass
+        if self._symbol_table.lookup(node.token.value) is None:
+            self._symbol_table.define(DeclaredSymbol(
+                name=node.token.value, value=self._current_line+1))
